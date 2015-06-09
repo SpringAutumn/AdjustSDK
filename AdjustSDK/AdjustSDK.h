c8cb9745f3e0abde127b7177734933d0bf6308aa
Wendeng Zhao <zhaowendeng@cienet.com.cn>
2015-06-09 19:09:22 +0800

    Carthage compatiable

---
+++ AdjustSDK/AdjustSDK.h
@@ -0,0 +1,40 @@
+//
+//  AdjustSDK.h
+//  AdjustSDK
+//
+//  Created by Winston Zhao on 6/9/15.
+//  Copyright (c) 2015 adjust GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AdjustSDK.
+FOUNDATION_EXPORT double AdjustSDKVersionNumber;
+
+//! Project version string for AdjustSDK.
+FOUNDATION_EXPORT const unsigned char AdjustSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AdjustSDK/PublicHeader.h>
+
+#import "Adjust.h"
+#import "ADJActivityHandler.h"
+#import "ADJActivityKind.h"
+#import "ADJActivityPackage.h"
+#import "ADJActivityState.h"
+#import "ADJAdjustFactory.h"
+#import "ADJLogger.h"
+#import "ADJPackageBuilder.h"
+#import "ADJPackageHandler.h"
+#import "ADJRequestHandler.h"
+#import "ADJTimer.h"
+#import "ADJUtil.h"
+#import "ADJEvent.h"
+#import "ADJDeviceInfo.h"
+#import "ADJAttributionHandler.h"
+#import "ADJAttribution.h"
+#import "ADJConfig.h"
+#import "NSString+ADJAdditions.h"
+#import "UIDevice+ADJAdditions.h"
+#import "NSData+ADJAdditions.h"
+
+
